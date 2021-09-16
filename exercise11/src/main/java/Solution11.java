5dda86ebaf012e257e9e3e90310fb81764ad3760
Gabriel Martin <gabriel.martin1202@gmail.com>
2021-09-15 22:39:09 -0400

    added exercise11

---
+++ exercise11/src/main/java/Solution11.java
@@ -0,0 +1,49 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution 11
+ *  Copyright 2021 Gabriel Martin
+ */
+
+
+import java.util.Scanner;
+
+/*
+ * declare usDollars, euros, rate as floats
+ * declare the scanner
+ * get the user input and store them into euros and rate
+ * use Float.parseFloat to change them from string to float
+ * usDollars = euros * rate
+ * print the output with the %.2f specifier to round to 2 decimal places
+ */
+public class Solution11 {
+    private static final Scanner input = new Scanner(System.in);
+    private static float usDollars;
+    private static float euros;
+    private static float rate;
+
+    public static void main(String[] args) {
+        userInput();
+        conversion();
+        printOutput();
+
+    }
+
+    private static void userInput() {
+        System.out.println("How many euros are you exchanging? ");
+        String input1 = input.nextLine();
+        euros = Float.parseFloat(input1);
+
+        System.out.println("What is the exchange rate? ");
+        String input2 = input.nextLine();
+        rate = Float.parseFloat(input2);
+
+    }
+
+    private static void conversion() {
+        usDollars = euros * rate;
+    }
+
+    private static void printOutput() {
+        System.out.printf("%.2f at an exchange rate of %f is%n%.2f U.S. dollars.", euros, rate, usDollars);
+    }
+
+}
