e8e4e0735bf4f31112c8911a4fe5bdb15d3db83a
Gabriel Martin <gabriel.martin1202@gmail.com>
2021-09-17 22:27:44 -0400

    added exercise16

---
+++ exercise16/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+X
+(src/main/java/assignment/Solution16.java,6\9\69cd60493fc4c79cd5e53bce456af36d362675b1
---
+++ exercise16/src/main/java/assignment/Solution16.java
@@ -0,0 +1,39 @@
+package assignment;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution 14
+ *  Copyright 2021 Gabriel Martin
+ */
+
+import java.util.Scanner;
+
+/*
+ * ask the user to input their age
+ * LEGAL_DRIVING_AGE = 16
+ * if userAge>=LEGAL_DRIVING_AGE
+ *      print that they can legally drive
+ * else
+ *      print they cannot legally drive
+ */
+public class Solution16 {
+    private static final int LEGAL_DRIVING_AGE = 16;
+    private static final Scanner input = new Scanner(System.in);
+    private int userAge;
+
+    public static void main(String[] args) {
+        Solution16 solution = new Solution16();
+        solution.userAge = solution.getUserAge();
+        solution.printIfLegal(solution.userAge);
+
+    }
+
+    public int getUserAge(){
+        System.out.println("What is your age: ");
+        return input.nextInt();
+    }
+
+    public void printIfLegal(int userAge) {
+        System.out.println(LEGAL_DRIVING_AGE <= userAge ? "You are legally old enough to drive!" : "You are not legally old enough to drive.");
+    }
+
+
+}
