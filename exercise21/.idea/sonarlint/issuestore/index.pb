62212b97df05acb644c8146b93b5fdd324a7b3f6
Gabriel Martin <gabriel.martin1202@gmail.com>
2021-09-19 00:18:49 -0400

    added exercise21

---
+++ exercise21/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+X
+(src/main/java/assignment/Solution21.java,8\6\8602347b5f095044d92b236b52b03f47e98fdb5f
---
+++ exercise21/src/main/java/assignment/Solution21.java
@@ -0,0 +1,65 @@
+package assignment;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution 21
+ *  Copyright 2021 Gabriel Martin
+ */
+
+import java.util.Scanner;
+
+/*
+ * ask the user for a month number
+ * use the month number to traverse through the switch cases
+ * assign monthString to the name of the month in case userInput
+ * print the name of the month after assigning
+ */
+public class Solution21 {
+    private static final Scanner input = new Scanner(System.in);
+    private String monthString;
+    private int month;
+
+    public static void main(String[] args) {
+        Solution21 solution = new Solution21();
+        solution.month = solution.userInputMonth();
+
+        solution.monthString = solution.monthToName(solution.month);
+
+        System.out.printf("The name of the month is %s", solution.monthString);
+    }
+
+    public int userInputMonth() {
+        System.out.println("Please enter the number of the month: ");
+        return input.nextInt();
+    }
+
+    public String monthToName(int month){
+        switch(month) {
+            case 1: monthString = "January";
+                break;
+            case 2: monthString = "February";
+                break;
+            case 3: monthString = "March";
+                break;
+            case 4: monthString = "April";
+                break;
+            case 5: monthString = "May";
+                break;
+            case 6: monthString = "June";
+                break;
+            case 7: monthString = "July";
+                break;
+            case 8: monthString = "August";
+                break;
+            case 9: monthString = "September";
+                break;
+            case 10: monthString = "October";
+                break;
+            case 11: monthString = "November";
+                break;
+            case 12: monthString = "December";
+                break;
+            default: monthString = "Invalid month.";
+                break;
+        }
+        return monthString;
+    }
+}
